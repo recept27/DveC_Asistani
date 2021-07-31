81e96b23782c3802d6ff94bdc0caca0aaed02f8f
receptas <recept462@gmail.com>
2021-07-31 11:56:02 +0300

    v1

--- lib/domain/truth_or_dare.dart
+++
@@ -1,4 +0,0 @@
-enum TruthOrDare {
-  truth,
-  dare,
-}
--- lib/shared/theme/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color blueBackground = Color(0xFF5DC6FE);
-  static const Color redBackground = Color(0xFFF25B5C);
-}
